a27a91589e60e6a447a1fe8286602f888ecf84e4
THANI-KHURSHID <thani.khurshid@gmail.com>
2020-04-22 15:35:15 +0500

    Added Clear

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
 let expression = "";
 let operatorCalled = false;
-
+let secondNumber = "";
 
 
 const display = document.getElementById("displayResult");
 const numberKeys = document.querySelectorAll(".calc_button");
 const operatorKeys = document.querySelectorAll(".calc_operator");
 const equalsKey = document.querySelector("#calc_equal");
+const clear = document.querySelector("#calc_clear");
 
 
 
@@ -14,6 +15,9 @@ function parse(str) {
   return Function(`'use strict'; return (${str})`)()
 }
 function populateDisplay(e) {
+  if(operatorCalled === true) {
+    secondNumber+=this.value;
+  }
   expression += this.value;
   display.value = expression;
 }
@@ -25,17 +29,25 @@ function setOperator(e) {
   expression += this.value;
   display.value = expression;
   console.log(`${operatorCalled}`);
+  secondNumber = "";
 }
 for (let j = 0; j < operatorKeys.length; j++) {
   operatorKeys[j].addEventListener("click", setOperator);
 }
 function getResult(e) {
-  if (operatorCalled) {
+  if (operatorCalled && secondNumber !== "" ) {
     let result = parse(expression);
     console.log(`${expression} = ${result} `);
     display.value = result;
+    expression = "";
   } else {
     return;
   }
 }
 equalsKey.addEventListener("click", getResult);
+function clearScreen() {
+  display.value = "";
+  expression = "";
+  secondNumber = "";
+} 
+clear.addEventListener("click" , clearScreen);
